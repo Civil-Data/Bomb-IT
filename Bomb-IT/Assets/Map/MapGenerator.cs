2a4a12fbb6690919e860884554168f4f53a37130
Felix Stockinger <felix.stockinger@yahoo.se>
2023-01-20 16:30:38 +0100

    Movement

--- Bomb-IT/Assets/Map/MapGenerator.cs
+++ Bomb-IT/Assets/Map/MapGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class MapGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
