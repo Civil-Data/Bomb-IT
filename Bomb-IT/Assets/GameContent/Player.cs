d3255705c4a0996d7d394147e1fe26521901cb3a
Felix Stockinger <felix.stockinger@yahoo.se>
2023-01-26 10:41:32 +0100

    Started work with PlaceItem script

--- Bomb-IT/Assets/GameContent/Player.cs
+++ Bomb-IT/Assets/GameContent/Player.cs
@@ -7,12 +7,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Bomb-IT/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
