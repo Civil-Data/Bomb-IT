e85b29c16ffea3d15706e0b8f83ea93a28ca094c
Lukasydkvist <ydlu21kc@student.ju.se>
2023-02-01 11:30:11 +0100

    Added audio mixer and volume setting to options menu

---
+++ Bomb-IT/Assets/GameContent/GameWindows/OptionsMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+    
+}
