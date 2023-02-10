8c76b66af5407b47110df0429a740583f7f54682
Lukasydkvist <ydlu21kc@student.ju.se>
2023-02-10 14:26:19 +0100

    Fixed using error

--- Assets/GameContent/GameWindows/MainMenu.cs
+++ Assets/GameContent/GameWindows/MainMenu.cs
@@ -1,10 +1,5 @@
-using Palmmedia.ReportGenerator.Core.Common;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
