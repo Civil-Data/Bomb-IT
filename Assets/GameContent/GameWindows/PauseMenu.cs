a4589a38872f67ee363b8902bfff33dba8530d5b
Martin Nilsson <martin.no.nilsson@gmail.com>
2023-02-16 20:49:52 +0100

    Added PauseMenu and fixed some map visuals

---
+++ Assets/GameContent/GameWindows/PauseMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject game;
+    private GameWindow gameWindow;
+
+    void Start()
+    {
+        game = GameObject.Find("Grid");
+        gameWindow = game.GetComponent<GameWindow>();
+    }
+
+    // Start is called before the first frame update
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; // Starts the game.
+        gameWindow.isPaused = false; // This is makes it able to resume to the game with button click.
+        SceneManager.UnloadSceneAsync("PauseMenu");
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("Meny");
+    }
+}
