ff513f7a2efdc6ec2c559768050fce4655407dd3
hanSomBorISKogen <joel-aannmo@hotmail.com>
2023-02-27 20:55:44 +0100

    Created a test that simulated that a scence was loaded.

--- Assets/GameLogic/Position.cs
+++
@@ -1,8 +0,0 @@
-public class Position
-{
-    public int Row { get; set; }
-
-    public int Col { get; set; }
-
-    public Position() { }
-}
--- Assets/Tests/EditMode/MapGeneratorTests.cs
+++ Assets/Tests/EditMode/MapGeneratorTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using UnityEngine.SceneManagement;
 
 public class MapGeneratorTests
 {
